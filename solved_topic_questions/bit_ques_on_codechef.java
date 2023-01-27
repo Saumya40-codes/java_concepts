921e1e9f0977458e48f94c607ef01b9fab4f6176
Saumya Shah <115284013+Saumya40-codes@users.noreply.github.com>
2023-01-27 22:23:07 +0530

    Update bit_ques_on_codechef.java

--- solved_topic_questions/bit_ques_on_codechef.java
+++ solved_topic_questions/bit_ques_on_codechef.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
-class CC{
+public class CC{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-		int N = in.nextInt();
-		for(int i = 0 ; i < N ; i++){
-		    int Q = in.nextInt();
-		    String bin = in.next();
-		    int count = 0;
-		    for(int j = 0 ; j < bin.length() ; j++){
-		        if(bin.charAt(j) == '1')
-		        count++;
-		    }
-		    if((count<=3 && count > 1) || (count == 1 && bin.length() >=3) )
-		    System.out.println("YES");
-		    else
-		    System.out.println("NO");
+	int N = in.nextInt();
+	for(int i = 0 ; i < N ; i++){
+	   int Q = in.nextInt();
+	   String bin = in.next();
+           int count = 0;
+	for(int j = 0 ; j < bin.length() ; j++){
+           if(bin.charAt(j) == '1')
+		count++;
+	}
+        if((count<=3 && count > 1) || (count == 1 && bin.length() >=3) )
+	      System.out.println("YES");
+        else
+	      System.out.println("NO");
     }
 }
 
@@ -46,4 +46,9 @@ NO
 YES
 YES
 NO
+*/
+/*
+Approach
+if number of ones in the given binary string is 2 or 3 then we can easily represent them in three of 2 powers form . In the case of one it is only possible if length 
+of string is >= 3. if ones are > 3 it wont be possible to arrange them in three of 2 powers form.
 */
