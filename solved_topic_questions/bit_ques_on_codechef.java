029622ca4634fae58a034f81c57b2040acad5aa3
Saumya40-codes <saumyabshah90@gmail.com>
2023-01-27 22:12:40 +0530

    upsolved codechef starters 45 bit question

---
+++ solved_topic_questions/bit_ques_on_codechef.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+class CC{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		for(int i = 0 ; i < N ; i++){
+		    int Q = in.nextInt();
+		    String bin = in.next();
+		    int count = 0;
+		    for(int j = 0 ; j < bin.length() ; j++){
+		        if(bin.charAt(j) == '1')
+		        count++;
+		    }
+		    if((count<=3 && count > 1) || (count == 1 && bin.length() >=3) )
+		    System.out.println("YES");
+		    else
+		    System.out.println("NO");
+    }
+}
+
+/*
+Question:
+Chef is given a number in form of its binary representation S, having length N.
+Determine if the number can be represented as a sum of exactly three non-negative powers of 2.Please refer to samples for further explanation.
+Note that S will NOT contain leading zeros.
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+Each test case consists of multiple lines of input.
+The first line of each test case contains a single integer 
+N, the length of the binary string.
+The next line contains the string S, the binary representation of a number.
+
+Sample 1:
+Input
+4
+1
+1
+4
+1001
+5
+11001
+7
+1101101
+Output
+NO
+YES
+YES
+NO
+*/
